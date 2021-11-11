6c3c1067e52fdb797e038d7ceee740d070beb187
tahaebaed <82852476+tahaebaed@users.noreply.github.com>
2021-11-11 18:11:11 +0200

    add some data to the db.json

--- src/app/Services/favorite-list.service.ts
+++ src/app/Services/favorite-list.service.ts
@@ -1,5 +1,5 @@
 import { IProdcut } from 'src/app/Shared Classes and types/iprodcut';
-import { Observable, observable } from 'rxjs';
+import { BehaviorSubject, Observable, observable, Subject } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
@@ -9,14 +9,19 @@ import { environment } from 'src/environments/environment';
 })
 export class FavoriteListService {
   favoriteList!: Observable<IProdcut[]>;
+  favArr!: any[];
+  favProdHolder: Subject<any[]>;
   httpOptions = {
     headers: new HttpHeaders({
       'content-type': 'application/json',
       accept: '*/*',
     }),
   };
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.favProdHolder = new BehaviorSubject([{}]);
+  }
 
+  
   accsesFavorite(): Observable<IProdcut[]> {
     this.favoriteList = this.http.get<IProdcut[]>(
       `${environment.apiUrl}/favorite`
